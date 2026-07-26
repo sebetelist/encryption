90c269007dfd509ab88bdb4f72971e460e5883a3
Alexander <sebetelist@protonmail.com>
2026-07-26 15:20:13 +0100

    Fix menu exit bug and improve error handling
    - Fixed exit() breaking the menu loop after encrypt/decrypt
    - Added UTF-8 encoding for GPG
    - Fixed misspelled words
    - Added empty input validation for input_data
    - Improved exception handling
    - Updated config
    - Replaced getpass with pwinput for masked password input
    - Added quit_program function
    - Minor improvements

--- generator.py
+++ generator.py
@@ -2,14 +2,17 @@ from pathlib import Path
 from itertools import count
 
 def get_unique_file(base_name, extension):
-    counter = count(1, 1)
+    folder = "files"
+    if not Path(folder).exists():
+        Path(folder).mkdir(parents=True, exist_ok=True)
+
+    counter = count(1)
     while True:
         num = next(counter)
-        file_path = Path(f"{base_name}_{num}{extension}")
-        
+        file_path = Path(folder) / f"{base_name}-{num}{extension}"
+
         if not file_path.exists():
             return file_path
-    
   
 
 
